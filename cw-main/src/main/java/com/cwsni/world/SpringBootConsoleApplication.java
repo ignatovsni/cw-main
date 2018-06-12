334806fda29248ae729fe1d6f8570e7b534bf0c9
ignatovsni <ignatovsni@gmail.com>
2018-06-11 18:13:02 -0700

    3

--- cw-main/src/main/java/com/cwsni/world/SpringBootConsoleApplication.java
+++ cw-main/src/main/java/com/cwsni/world/SpringBootConsoleApplication.java
@@ -26,7 +26,7 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
         app.setWebApplicationType(WebApplicationType.NONE);
         app.setBannerMode(Banner.Mode.LOG);
         app.run(args);
-        // test change
+        // test change - 2
         // git@github.com:ignatovsni/cw-main.git
 
     }
