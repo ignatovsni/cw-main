1f7dae42fb25f7b991536cef74d30df904c05eeb
ignatovsni <ignatovsni@gmail.com>
2019-05-23 22:37:50 -0700

    and here we go again

---
+++ cw-matrix/src/main/java/com/cwsni/cw/matrix/CwMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.cwsni.cw.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwMatrixApplication.class, args);
+	}
+
+}
